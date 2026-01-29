fdf3af0fb5f9e37349ce8a254f73894249a3439a
Kevin <km9530802@gmail.com>
2026-01-29 21:15:28 +0100

    feat: support ida_settings

--- plugin/virustotal/config.py
+++ plugin/virustotal/config.py
@@ -4,3 +4,15 @@
 API_KEY = ''  # Mandatory for CodeInsight
 DEBUG = False
 
+try:
+    import ida_settings
+
+    vt_api_key = ida_settings.get_current_plugin_setting("api_key")
+    if vt_api_key:
+        API_KEY = vt_api_key
+
+    vt_debug = ida_settings.get_current_plugin_setting("debug")
+    if vt_debug is not None:
+        DEBUG = bool(vt_debug)
+except:
+    pass
